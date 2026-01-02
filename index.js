1950c22bc4062edab4b3480e0074430c11fe4b4a
NIKHIL <pppe@gmail.com>
2026-01-02 18:12:16 +0545

    added a restart Button

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let button = document.getElementById('Button1');
-let counter = document.getElementById('counter');
+let button1 = document.getElementById('Button1');
+let button2 = document.getElementById('Button2');
+let counter = document.getElementsByClassName('Counter');
 
 let count = 0;
-
+ 
 function increment(){
-  count += 1;
-
-  counter.innerHTML = count;
+  count++;
+  for(let i = 0; i < counter.length ; i++){
+    counter[i].innerHTML = count;
+  }
 }
 
-button.addEventListener('click' , increment);
+function Restart(){
+  count = 0;
+  for(let i = 0; i < counter.length ; i++)
+    counter[i].innerHTML = count;
+}
+
+button1.addEventListener('click' , increment);
+button2.addEventListener('click' , Restart);
